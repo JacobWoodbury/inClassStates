6c7ee631d29fe8b0ffaa9a6d2e4a6d5af82a496f
Jacob Woodbury <jacobswoodbury@gmail.com>
2025-10-16 14:25:42 -0700

    planting working ish

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,33 +1,31 @@
-import React from "react"
+import React from "react";
 
 export default function Game(){
     const seedling = <a href="https://www.flaticon.com/free-icons/sprout" title="sprout icons"></a>
-    const [bagItems, setBagItems] = React.useState(["wheat"]);
-    const [farmItems, setFarmItems] = React.useState(["","","",""]);
+    const [bagItems, setBagItems] = React.useState(["wheat", "flower"]);
+    const [farmItems, setFarmItems] = React.useState([]);
     const [currentSeed, setCurrentSeed] = React.useState("Wheat");
 
+    const mappedFarmItems = farmItems.map((item) => (plant(item)))
     function addSeed() {
         
         setFarmItems((prev) => {
-            [...prev, currentSeed]
+           return([...prev, currentSeed])
         })
             
         
     }   
     function plant(current){
-        if(current === ""){
-            return(<li key={Date.now()}><button className="plantButton" onClick={(addSeed)}>Plant Here</button></li>)
-        }else{
-            return(<li key={Date.now()}>{current}</li>)
-        }
+        return(<li>{current}</li>)
     }
-    const mappedFarmItems = farmItems.map((item) => (plant(item)))
+    
     
     return(
         <>
             <div className="description">This is our game, click a square to plant in it.</div>
 
             <section className="farm">
+                <button className="plantButton" onClick={addSeed}>Plant a seed</button>
                 {mappedFarmItems}
                 
             </section>
