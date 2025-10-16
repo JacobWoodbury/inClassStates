e6db688e1664ed928580f9a6f1d7e495b8236591
Jacob Woodbury <jacobswoodbury@gmail.com>
2025-10-16 14:10:17 -0700

    error testing

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -3,42 +3,33 @@ import React from "react"
 export default function Game(){
     const seedling = <a href="https://www.flaticon.com/free-icons/sprout" title="sprout icons"></a>
     const [bagItems, setBagItems] = React.useState(["wheat"]);
-    const [farmItems, setFarmItems] = React.useState([]);
-    const [currentSeed, setCurrentSeed] = React.useState();
+    const [farmItems, setFarmItems] = React.useState(["","","",""]);
+    const [currentSeed, setCurrentSeed] = React.useState("Wheat");
 
     function addSeed() {
-        if (currentSeed) {
-            if (currentSeed === "Wheat") {
-                setFarmItems((prev) => {
-                    [...prev, currentSeed]
-                })
-            }
-        }
+        
+        setFarmItems((prev) => {
+            [...prev, currentSeed]
+        })
+            
+        
     }   
-
-    const mappedFarmItems = farmItems.map((item) => {
-        return (
-            <>
-                <div>
-                    {item}
-                </div>
-            </>
-        )
-    })
-
+    function plant(current){
+        if(current === ""){
+            return(<li key={Date.now()}><button className="plantButton" onClick={(addSeed)}>Plant Here</button></li>)
+        }else{
+            return(<li key={Date.now()}>{current}</li>)
+        }
+    }
+    const mappedFarmItems = farmItems.map((item) => (plant(item)))
+    
     return(
         <>
             <div className="description">This is our game, click a square to plant in it.</div>
 
             <section className="farm">
-                <div className="farmRow">
-                    <button>Plant here!</button>
-                    <button>Plant here!</button>
-                </div>
-                <div className="farmRow">
-                    <button>Plant here!</button>
-                    <button>Plant here!</button>
-                </div>
+                {mappedFarmItems}
+                
             </section>
             <section className="bag">
                 <ul>
