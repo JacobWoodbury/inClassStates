e537df2425f5f69cbfd278f0b6ed94c2dd41d4ef
Jacob Woodbury <jacobswoodbury@gmail.com>
2025-10-16 13:17:16 -0700

    setting up board

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,6 +1,34 @@
+import React from "react"
+
 export default function Game(){
+    const seedling = <a href="https://www.flaticon.com/free-icons/sprout" title="sprout icons"></a>
+    const [bagItems, setBagItems] = React.useState(["wheat"]);
+    const [farmItems, setFarmItems] = React.useState([])
+
+    
     return(
         <>
+            <div className="description">This is our game, click a square to plant in it.</div>
+
+            <section className="farm">
+                <div className="farmRow">
+                    <button></button>
+                    <button></button>
+                </div>
+                <div className="farmRow">
+                    <button></button>
+                    <button></button>
+                </div>
+            </section>
+            <section className="bag">
+                <ul>
+
+                </ul>
+            </section>
+
+
+
+
         </>
     )
 }
